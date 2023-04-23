8f8463d443454b08ed4301cf1e70465703f43ec3
FarHowl <kartashov104@gmail.com>
2023-04-23 10:17:33 +0300

    s

--- .garden/build/books-service/src/utils/verifyJWT.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const verifyJWT = async (req) => {
-    const token = req.headers.authorization.split(" ")[1];
-
-    jwt.verify(token, process.env.publicKeyForUsers);
-};
-
-module.exports = verifyJWT;
